ce9d4fa9931a1b6142803375501d836663c5bead
Dronchillah <akurbatov2002@gmail.com>
2022-08-18 00:15:15 +0300

    1

--- src/main/java/ua/dronchillah/Actions.java
+++ src/main/java/ua/dronchillah/Actions.java
@@ -23,9 +23,28 @@ public class Actions {
         }
     }
 
+    protected static void viewCurrentEntry(int id){
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/main?serverTimezone=UTC",
+                "root", "PizzaEater228")){
+            PreparedStatement viewTableStatement = connection.prepareStatement("SELECT * FROM main.users WHERE id = ?");
+            viewTableStatement.setInt(1, id);
+            ResultSet set = viewTableStatement.executeQuery();
+            ResultSetMetaData rsmd = set.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (set.next()) {
+                for(int i = 1 ; i <= columnsNumber; i++){
+                    System.out.print(set.getString(i) + " ");
+                }
+                System.out.println();
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
     protected static void create(String username, String password, String email){
         try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/main?serverTimezone=UTC",
-                "root", "PizzaEater228"); Scanner scan = new Scanner(System.in)){
+                "root", "PizzaEater228")){
             PreparedStatement createStatement = connection.prepareStatement("INSERT INTO main.users (username, password, email) VALUES (?, ?, ?)");
             createStatement.setString(1, username);
             createStatement.setString(2, password);
@@ -35,5 +54,52 @@ public class Actions {
             throw new RuntimeException(e);
         }
         viewAllEntries();
+    }
+
+    protected static void delete(int i){
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/main?serverTimezone=UTC",
+                "root", "PizzaEater228")){
+            PreparedStatement createStatement = connection.prepareStatement("DELETE FROM main.users WHERE id = ?");
+            createStatement.setInt(1, i);
+            createStatement.execute();
+        }catch (SQLException e){
+            throw  new RuntimeException(e);
+        }
+    }
+
+    protected static boolean isRecordAvailable(int recordId){
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/main?serverTimezone=UTC",
+                "root", "PizzaEater228")){
+            PreparedStatement statement = connection.prepareStatement("SELECT COUNT(id) as id FROM main.users WHERE id = ?");
+            statement.setInt(1, recordId);
+            ResultSet set = statement.executeQuery();
+            set.next();
+            return set.getInt(1) != 0;
+        }catch (SQLException e){
+            throw  new RuntimeException(e);
+        }
+
+    }
+
+    protected static void update(int id, String option, String value){
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/main?serverTimezone=UTC",
+                "root", "PizzaEater228")){
+            PreparedStatement createStatement;
+            if(option.equals("username")){
+                createStatement = connection.prepareStatement("UPDATE main.users SET username = ? WHERE id = ?");
+            } else if (option.equals("password")){
+                createStatement = connection.prepareStatement("UPDATE main.users SET password = ? WHERE id = ?");
+            } else if (option.equals("email")){
+                createStatement = connection.prepareStatement("UPDATE main.users SET email = ? WHERE id = ?");
+            } else {
+                throw new SQLException();
+            }
+            createStatement.setString(1, value);
+            createStatement.setString(2, id + "");
+            createStatement.execute();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        viewCurrentEntry(id);
     }
 }
--- src/main/java/ua/dronchillah/ConsoleMenu.java
+++ src/main/java/ua/dronchillah/ConsoleMenu.java
@@ -1,36 +1,34 @@
 package ua.dronchillah;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ConsoleMenu {
-    static Scanner scan = new Scanner(System.in);
+    final static Scanner scan = new Scanner(System.in);
     public static void start() {
-        String menu = "Welcome to Dronchillah's CRUD application " +
-                "\nChoose one of next options: " +
+        String menu = "\nChoose one of next options: " +
                 "\n 1.View all entries" +
                 "\n 2.Create entry" +
                 "\n 3.Update entry" +
                 "\n 4.Delete entry" +
                 "\n 5.Stop application";
-        System.out.println(menu);
-        try {
+        String startMenu = "Welcome to Dronchillah's CRUD application " + menu;
+        System.out.println(startMenu);
+            String input = scan.nextLine();
             bigLoopa:
-            while (true) {
-                String str = scan.nextLine();
-                switch (str) {
+            while (!input.equalsIgnoreCase("q")) {
+                switch (input) {
                     case "1":
-                        System.out.println("case 1");
-                        Actions.viewAllEntries();
+                        viewAllEntries();
                         break;
                     case "2":
-                        System.out.println("case 2");
                         create();
                         break;
                     case "3":
-                        System.out.println("case 3");
+                        update();
                         break;
                     case "4":
-                        System.out.println("case 4");
+                        delete();
                         break;
                     case "5":
                         System.out.println("bye");
@@ -38,46 +36,105 @@ public class ConsoleMenu {
                     default:
                         System.out.println("Choose correct number of option:");
                 }
+                System.out.println(menu);
+                input = scan.nextLine();
             }
-        } finally {
-            scan.close();
-        }
     }
-    private static void viewAllEntries(){
 
+    private static void viewAllEntries() {
+        Actions.viewAllEntries();
     }
-    private static void create(){
-        //Scanner scanner = new Scanner(System.in);
-            System.out.println("Input username:");
-            String username = scan.nextLine();
-            System.out.println("Input password:");
-            String password = scan.nextLine();
-            System.out.println("Input email:");
-            String email = checkEmail(scan.nextLine());
-            Actions.create(username, password, email);
 
+    private static void create() {
+        System.out.println("Input username:");
+        String username = scan.nextLine();
+        System.out.println("Input password:");
+        String password = scan.nextLine();
+        System.out.println("Input email:");
+        String email = checkEmail(scan.nextLine());
+        Actions.create(username, password, email);
     }
+
     private static void update(){
+        viewAllEntries();
+        System.out.println("Please, choose number of record to edit:");
+        int id = checkId(scan.nextLine());
+        System.out.println("Chosen record:");
+        Actions.viewCurrentEntry(id);
+        System.out.println("Choose option to edit: " +
+                "\n1.Edit username" +
+                "\n2.Edit password" +
+                "\n3.Edit email" +
+                "\n4.Quit");
+        String input = scan.nextLine();
+        bigLoopa:
+        while (!input.equalsIgnoreCase("q")) {
+            switch (input) {
+                case "1":
+                    System.out.println("Please, input new username:");
+                    String username = scan.nextLine();
+                    Actions.update(id, "username", username);
+                    break bigLoopa;
+                case "2":
+                    System.out.println("Please, input new password:");
+                    String password = scan.nextLine();
+                    Actions.update(id, "password", password);
+                    break bigLoopa;
+                case "3":
+                    System.out.println("Please, input new email:");
+                    String email = checkEmail(scan.nextLine());
+                    Actions.update(id, "email", email);
+                    break bigLoopa;
+                case "4":
+                    System.out.println("bye");
+                    break bigLoopa;
+                default:
+                    System.out.println("Choose correct number of option:");
+            }
+            input = scan.nextLine();
+        }
 
     }
-    private static void delete(){
+
+    private static void delete() {
+        viewAllEntries();
+        System.out.println("Input id of user you want to delete:");
+        Actions.delete(checkId(scan.nextLine()));
 
     }
 
-    private static String checkEmail(String str){
+    private static String checkEmail(String str) {
         String temp;
-        try(Scanner scan = new Scanner(System.in)){
-            if(str.matches("[a-zA-Z\\d]+@[a-z]+\\.[a-z]+")){
+        try (Scanner scan = new Scanner(System.in)) {
+            if (str.matches("[a-zA-Z\\d]+@[a-z]+\\.[a-z]+")) {
                 return str;
             } else {
                 while (true) {
                     System.out.println("Please, input the correct email:");
                     temp = scan.nextLine();
-                    if(temp.matches("[a-zA-Z\\d]+@[a-z]+\\.[a-z]+")){
+                    if (temp.matches("[a-zA-Z\\d]+@[a-z]+\\.[a-z]+")) {
                         return temp;
                     }
                 }
             }
+        }
+    }
+
+    private static int checkId(String id){
+        while(true){
+            try{
+                int intId = Integer.parseInt(id);
+                if(!Actions.isRecordAvailable(intId)){
+                    System.out.println("Please, input correct id:");
+                } else {
+                    return intId;
+                }
+            }catch (NumberFormatException e){
+                System.out.println("Please, input correct id:");
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+            id = scan.nextLine();
         }
     }
 }
